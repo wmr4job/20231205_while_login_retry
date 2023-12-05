ce33e43e36358e14c00acb878c9592db1eea65d5
Milly <wmr4job@gmail.com>
2023-12-05 18:14:32 +0800

    Update way2

--- while_login_retry2.py
+++ while_login_retry2.py
@@ -1,15 +1,14 @@
 # while_login_retry_way2
 password = 'A12345' #預設密碼
-user = input('請輸入密碼：')
-n = 2 #剩餘可登入次數
-while True:
+
+n = 3 #剩餘可登入次數
+while n > 0:
+	user = input('請輸入密碼：')
 	if user == password:
 		print('登入成功！')
 		break #跳出迴圈
-	elif n == 0:
-		print('密碼連續錯誤3次，請稍後再試！')
-		break
 	else:
-		print('密碼錯誤！還有', n, '次機會')
-		user = input('請輸入密碼：')
 		n -= 1
+		print('密碼錯誤！還有', n, '次機會')
+if n == 0:
+	print('密碼連續錯誤3次，請稍後再試！')
